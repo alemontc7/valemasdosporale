6e9fe805b0c44e0f90311b5257d494c2c2ecd30d
Ale Castro <alejandro.castro.0520@gmail.com>
2024-08-02 18:27:20 -0400

    Prueba

---
+++ code/graph/bfs.cpp
@@ -0,0 +1,18 @@
+vector<vi> gr(n);
+queue<int> q;
+vector<bool> vis(n, 0);
+q.push(0);
+vis[0] = 1;
+while (q.size())
+{
+  int node = q.front();
+  q.pop();
+  forn(auto v : gr[node])
+  {
+    if (!vis[v])
+    {
+      q.push(v);
+      vis[v] = 1;
+    }
+  }
+}
